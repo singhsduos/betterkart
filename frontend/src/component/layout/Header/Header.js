baef7039fee8dfae35147132ca7eab1c05f9c652
singhsduos <ns9628491678@gmail.com>
2022-01-22 20:37:07 +0530

    new NavBar added

--- frontend/src/component/layout/Header/Header.js
+++ frontend/src/component/layout/Header/Header.js
@@ -1,45 +1,87 @@
-import React from 'react';
+import React, { useState } from "react";
 import logo from "../../../images/logo.png";
-
-const options = {
-    // burgerColor: "white",
-    burgerColorHover: "#eb4034",
-    logo,
-    logoWidth: "20vmax",
-    navColor1: "white",
-    logoHoverSize: "10px",
-    logoHoverColor: "#eb4034",
-    link1Text: "Home",
-    link2Text: "Products",
-    link3Text: "Contact",
-    link4Text: "About",
-    link1Url: "/",
-    link2Url: "/products",
-    link3Url: "/contact",
-    link4Url: "/about",
-    link1Size: "1.3vmax",
-    link1Color: "rgba(35, 35, 35,0.8)",
-    nav1justifyContent: "flex-end",
-    nav2justifyContent: "flex-end",
-    nav3justifyContent: "flex-start",
-    nav4justifyContent: "flex-start",
-    link1ColorHover: "#eb4034",
-    link1Margin: "1vmax",
-    profileIconUrl: "/login",
-    profileIconColor: "rgba(35, 35, 35,0.8)",
-    searchIconColor: "rgba(35, 35, 35,0.8)",
-    cartIconColor: "rgba(35, 35, 35,0.8)",
-    profileIconColorHover: "#eb4034",
-    searchIconColorHover: "#eb4034",
-    cartIconColorHover: "#eb4034",
-    cartIconMargin: "1vmax",
-
-};
+import "./SCSS/Header.css";
+import { FaUserCircle } from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { BsSearch, BsFillCartCheckFill } from "react-icons/bs";
+import { ImCross } from "react-icons/im";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
-    return <>
-        <h1>Hello</h1>
-    </>;
+    const [showMediaIcons, setShowMediaIcons] = useState(false);
+
+    return (
+        <>
+
+            <div className="menuBurger">
+
+                <a href="#" onClick={() => setShowMediaIcons(!showMediaIcons)}>
+                    <GiHamburgerMenu />
+                </a>
+
+            </div>
+
+            <nav className={
+                showMediaIcons ? "main-nav open" : "main-nav close"
+            }>
+
+                {/* 1st logo part  */}
+                <div className="logo">
+                    <NavLink onClick={() => setShowMediaIcons(!showMediaIcons)} to="/">
+                        <img src={logo} alt="BetterKart" />
+                    </NavLink>
+                </div>
+
+                {/* 2nd menu part  */}
+                <div className="menu-link"
+                >
+                    <ul>
+                        <div>
+                            <li>
+                                <NavLink to="/" onClick={() => setShowMediaIcons(!showMediaIcons)}>Home</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/products" onClick={() => setShowMediaIcons(!showMediaIcons)}>Products</NavLink>
+                            </li>
+                        </div>
+                        <div>
+                            <li>
+                                <NavLink to="/contact" onClick={() => setShowMediaIcons(!showMediaIcons)}>Contact</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/about" onClick={() => setShowMediaIcons(!showMediaIcons)}>About</NavLink>
+                            </li>
+                        </div>
+                    </ul>
+                </div>
+
+                {/* 3rd social media links */}
+                <div className="social-media">
+                    <ul className="social-media-desktop">
+                        <li>
+                            <NavLink onClick={() => setShowMediaIcons(!showMediaIcons)} to="/search">
+                                <BsSearch className="facebook" />
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink onClick={() => setShowMediaIcons(!showMediaIcons)} to="/cart">
+                                <BsFillCartCheckFill className="facebook" />
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink onClick={() => setShowMediaIcons(!showMediaIcons)} to="/login">
+                                <FaUserCircle className="facebook" />
+                            </NavLink>
+                        </li>
+                    </ul>
+
+
+                </div>
+            </nav>
+        </>
+    );
 };
+
+
 
 export default Header;
