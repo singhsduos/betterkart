82a88f5baa14cfe7fce56bcdab18236855390a22
singhsduos <ns9628491678@gmail.com>
2022-01-29 00:10:49 +0530

    favicon-icon

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,8 @@ import App from './App';
 import {Provider} from 'react-redux';
 import store from './store';
 import { positions, transitions, Provider as AlertProvider } from 'react-alert';
-import AlertTemplate  from 'react-alert-template-basic';
+import AlertTemplate from 'react-alert-template-basic';
+import Favicon from 'react-favicon';
 
 // Alert Message CSS
 const options = {
@@ -18,6 +19,7 @@ ReactDOM.render(
   <Provider store={store}>
     
     <AlertProvider template={AlertTemplate} {...options}>
+      <Favicon url='https://user-images.githubusercontent.com/32032008/151602278-be08dd08-594a-44e0-9451-1cbe3a009fbe.jpeg' />
       <App />
     </AlertProvider>
 
